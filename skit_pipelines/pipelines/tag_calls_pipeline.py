687ddc38635f79f471f63962b6f27036d8f4af89
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2022-03-25 02:11:02 +0530

    add: tag-calls pipeline

---
+++ skit_pipelines/pipelines/tag_calls_pipeline.py
@@ -0,0 +1,21 @@
+import kfp
+
+from skit_pipelines import constants as pipeline_constants
+from skit_pipelines.components import auth, org_auth_token_op, tag_calls_op, slack_notification_op
+
+
+@kfp.dsl.pipeline(
+    name="Tag Calls Pipeline",
+    description="Uploads calls to database for tagging",
+)
+def run_tag_calls(
+    org_id: int,
+    job_id: int,
+    s3_path: str
+):
+    auth_token = org_auth_token_op(org_id)
+    tag_calls_op(
+        input_file=s3_path,
+        job_id=job_id,
+        token=auth_token.output,
+    )
