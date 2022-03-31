83f5cd29ac2fac839176241b523d7560948a6946
ltbringer <amresh.venugopal@gmail.com>
2022-03-31 15:31:22 +0530

    add: component for training intent classifier WIP.

---
+++ skit_pipelines/components/train_voicebot_xlmr.py
@@ -0,0 +1,42 @@
+import kfp
+from kfp.components import InputPath
+
+from skit_pipelines import constants as pipeline_constants
+
+
+def train_xlmr_voicebot(
+    data_path: InputPath,
+    utterance_column: str,
+    label_column: str,
+    state_column: str,
+    use_state: str,
+    model_type: str = "xlmroberta",
+    model_name: str = "xlm-roberta-base",
+):
+    import pandas as pd
+    from simpletransformers.classification import (
+        ClassificationArgs,
+        ClassificationModel,
+    )
+    from sklearn import preprocessing
+
+    label_column = pipeline_constants.VOICE_BOT_XLMR_LABEL_COL
+    train_df = pd.read_csv(data_path)
+    labelencoder = preprocessing.LabelEncoder()
+    encoder = labelencoder.fit(train_df[label_column])
+    model_args = ClassificationArgs(num_train_epochs=1)
+
+    train_df[:, label_column] = encoder.transform(train_df[label_column])
+
+    model = ClassificationModel(
+        model_type,
+        model_name,
+        num_labels=train_df[label_column].nunique(),
+        args=model_args,
+    )
+    model.train_model(train_df)
+
+
+train_xlmr_voicebot_op = kfp.components.create_component_from_func(
+    train_xlmr_voicebot, base_image=pipeline_constants.BASE_IMAGE
+)
