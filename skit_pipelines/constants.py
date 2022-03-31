708312db88e1f1be6bc0477b14d51b4a9e31ee6c
ltbringer <amresh.venugopal@gmail.com>
2022-03-31 15:30:16 +0530

    add: placeholders for training intent classifier.

--- skit_pipelines/constants.py
+++ skit_pipelines/constants.py
@@ -13,3 +13,6 @@ DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_USER = os.environ["DB_USER"]
+VOICE_BOT_XLMR_LABEL_COL = ""
+TEXT = ""
+LABELS = ""
