7dc766cbf6dc659b2aaa44601178486394ba58b3
ltbringer <amresh.venugopal@gmail.com>
2022-04-01 20:28:05 +0530

    add: processing df within training component.

--- skit_pipelines/components/train_voicebot_xlmr.py
+++ skit_pipelines/components/train_voicebot_xlmr.py
@@ -1,6 +1,5 @@
 import kfp
 from kfp.components import InputPath
-
 from skit_pipelines import constants as pipeline_constants
 
 
@@ -8,8 +7,6 @@ def train_xlmr_voicebot(
     data_path: InputPath,
     utterance_column: str,
     label_column: str,
-    state_column: str,
-    use_state: str,
     model_type: str = "xlmroberta",
     model_name: str = "xlm-roberta-base",
 ):
@@ -19,6 +16,7 @@ def train_xlmr_voicebot(
         ClassificationModel,
     )
     from sklearn import preprocessing
+    from skit_pipelines import constants as pipeline_constants
 
     label_column = pipeline_constants.VOICE_BOT_XLMR_LABEL_COL
     train_df = pd.read_csv(data_path)
@@ -27,7 +25,10 @@ def train_xlmr_voicebot(
     model_args = ClassificationArgs(num_train_epochs=1)
 
     train_df[:, label_column] = encoder.transform(train_df[label_column])
-
+    train_df.rename(
+        columns={utterance_column: pipeline_constants.TEXT, label_column: pipeline_constants.LABELS},
+        inplace=True,
+    )
     model = ClassificationModel(
         model_type,
         model_name,
