b9904f162a62c3c9a1173be642f27d4816228112
ltbringer <amresh.venugopal@gmail.com>
2022-04-01 20:27:10 +0530

    add: component to download from s3.

---
+++ skit_pipelines/components/download_from_s3.py
@@ -0,0 +1,26 @@
+import kfp
+from kfp.components import InputPath, OutputPath
+
+from skit_pipelines import constants as pipeline_constants
+
+
+def download_from_s3(
+    s3_path: str,
+    path_on_disk: OutputPath(str)
+) -> None:
+    import re
+    import boto3
+    from loguru import logger
+
+    pattern = re.compile(r"^s3://(.+?)/(.+?)$")
+    logger.debug(f"{s3_path=}")
+    bucket, key = pattern.match(s3_path).groups()
+    logger.debug(f"{bucket=} {key=}")
+
+    s3_resource = boto3.client("s3")
+    s3_resource.download_file(bucket, key, path_on_disk)
+
+
+download_from_s3_op = kfp.components.create_component_from_func(
+    download_from_s3, base_image=pipeline_constants.BASE_IMAGE
+)
