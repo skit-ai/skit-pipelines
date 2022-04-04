2a3d85763581f4055f56fa15523f5e7d3e2fb92a
ltbringer <amresh.venugopal@gmail.com>
2022-04-04 19:46:19 +0530

    update: labelencoder transform.

--- skit_pipelines/components/train_voicebot_xlmr.py
+++ skit_pipelines/components/train_voicebot_xlmr.py
@@ -28,18 +28,17 @@ def train_xlmr_voicebot(
     from sklearn import preprocessing
 
     from skit_pipelines import constants as pipeline_constants
+    # train model
 
-    label_column = pipeline_constants.VOICE_BOT_XLMR_LABEL_COL
     train_df = pd.read_csv(data_path)
     labelencoder = preprocessing.LabelEncoder()
     encoder = labelencoder.fit(train_df[label_column])
     model_args = ClassificationArgs(num_train_epochs=1)
 
-    train_df[:, label_column] = encoder.transform(train_df[label_column])
+    train_df[pipeline_constants.LABELS] = encoder.transform(train_df[label_column])
     train_df.rename(
         columns={
             utterance_column: pipeline_constants.TEXT,
-            label_column: pipeline_constants.LABELS,
         },
         inplace=True,
     )
