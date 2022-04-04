516e0df9b9a1660aaf49e2b3548230e2d20c0c90
ltbringer <amresh.venugopal@gmail.com>
2022-04-05 02:13:47 +0530

    add: support for uploading a dir.

--- skit_pipelines/components/upload2s3.py
+++ skit_pipelines/components/upload2s3.py
@@ -11,17 +11,29 @@ def upload2s3(
     bucket: str,
     ext: str = ".csv",
 ) -> str:
+    import os
+    from glob import glob
+
     import boto3
     from loguru import logger
 
-    from skit_pipelines.utils import create_file_name
+    from skit_pipelines.utils import create_file_name, create_dir_name
 
     s3_resource = boto3.client("s3")
-    upload_path = create_file_name(org_id, file_type, ext)
-    s3_resource.upload_file(path_on_disk, bucket, upload_path)
-    logger.debug(f"Uploaded {path_on_disk} to {upload_path}")
+
+    if os.path.isfile(path_on_disk):
+        upload_path = create_file_name(org_id, file_type, ext)
+        s3_resource.upload_file(path_on_disk, bucket, )
+    elif os.path.isdir(path_on_disk):
+        upload_path = create_dir_name(org_id, file_type)
+        for full_file_path in glob(f"{path_on_disk}/**"):
+            _, file_path = os.path.split(full_file_path)
+            s3_resource.upload_file(full_file_path, bucket, os.path.join(upload_path, file_path))
+
     s3_path = f"s3://{bucket}/{upload_path}"
+    logger.debug(f"Uploaded {path_on_disk} to {upload_path}")
     return s3_path
+
 
 
 upload2s3_op = kfp.components.create_component_from_func(
