d388a64439a75655afb4c5b2b44e9e32bf5d8cfe
ltbringer <amresh.venugopal@gmail.com>
2022-05-28 15:34:28 +0530

    add: generic schema mapped to pipeline name.

--- skit_pipelines/api/models/__init__.py
+++ skit_pipelines/api/models/__init__.py
@@ -1,15 +1,13 @@
 from skit_pipelines.api.models.requests import (
-    BaseRequestSchema, FetchCallSchema, TagCallSchema, TrainModelSchema, StorageOptions
+    BaseRequestSchema, FetchCallSchema, TagCallSchema, TrainModelSchema, StorageOptions, get_all_pipelines_fn, generate_schema
 )
 from skit_pipelines.api.models.responses import customResponse, statusWiseResponse, successfulCreationResponse
 from skit_pipelines.api.models.custom_models import ParseRunResponse
 
 import skit_pipelines.api.models.errors as errors
 
-from typing import Union
 
-ValidRequestSchemas = Union[
-    FetchCallSchema,
-    TagCallSchema,
-    TrainModelSchema,
-]
+RequestSchemas = {
+    pipeline_name: generate_schema(pipeline_name, pipeline_fn)
+    for pipeline_name, pipeline_fn in get_all_pipelines_fn().items()
+}
