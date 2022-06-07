8660a90701bb2bfa276fab936dfdd7fc4bfb57c3
ltbringer <amresh.venugopal@gmail.com>
2022-06-07 18:36:32 +0530

    update: accomodate tag style b/w tog and labelstudio.

--- skit_pipelines/components/preprocess/create_true_intent_column/utils.py
+++ skit_pipelines/components/preprocess/create_true_intent_column/utils.py
@@ -2,6 +2,9 @@ import json
 
 
 def pick_1st_tag(tag: str):
-    tag = json.loads(tag)
-    tag, *_ = json.loads(tag) if isinstance(tag, str) else tag
-    return tag.get("type")
+    try:
+        tag = json.loads(tag)
+        tag, *_ = json.loads(tag) if isinstance(tag, str) else tag
+        return tag.get("type")
+    except json.JSONDecodeError:
+        return tag
