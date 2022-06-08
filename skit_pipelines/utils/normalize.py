2b068b67957fa61e170b52e875388e00b30dea6d
ltbringer <amresh.venugopal@gmail.com>
2022-06-09 03:15:07 +0530

    add: method to support comma separated strings.

--- skit_pipelines/utils/normalize.py
+++ skit_pipelines/utils/normalize.py
@@ -6,3 +6,17 @@ def to_camel_case(name: str) -> str:
     words = to_snake_case(name).split("_")
     title_cased = map(lambda word: word.title(), words)
     return "".join(title_cased)
+
+
+def strip(c: str) -> str:
+    return c.strip()
+
+
+def isdigit(c: str) -> bool:
+    return c.isdigit()
+
+
+def comma_sep_str(string: str, fn = None) -> str:
+    default_fn = lambda c: c
+    fn = fn or default_fn
+    return list(map(fn, filter(isdigit, map(strip, string.split(",")))))
