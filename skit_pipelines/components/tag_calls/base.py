e31bd200e87e24895b1c167d532bae798e4214b1
ltbringer <amresh.venugopal@gmail.com>
2022-06-09 03:14:47 +0530

    refactor: move methods to tag_calls module.

---
+++ skit_pipelines/components/tag_calls/base.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+Response = namedtuple("Response", "errors df_sizes")
---
+++ skit_pipelines/components/tag_calls/tog.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from skit_labels.cli import upload_dataset
+from skit_labels import constants as labels_constants
+from skit_pipelines import constants as pipeline_constants
+from skit_pipelines.components.tag_calls.base import Response
+
+
+def upload2tog(input_file: str, url: str, token: str, job_ids: List[str], response: Response):
+    url = pipeline_constants.CONSOLE_API_URL
+    for job_id in job_ids:
+        errors, df_size = upload_dataset(
+            input_file, url, token, job_id, labels_constants.SOURCE__DB
+        )
+    if errors:
+        response.errors.extend(errors)
+        response.df_sizes.append(df_size)
+    return response
