ab06b6e498ccc225111e6d9a762aba9dc84271c7
ltbringer <amresh.venugopal@gmail.com>
2022-06-09 13:56:53 +0530

    update: enable uploading s3 paths directly.

--- skit_pipelines/components/tag_calls/labelstudio.py
+++ skit_pipelines/components/tag_calls/labelstudio.py
@@ -1,12 +1,16 @@
+import tempfile
 from skit_labels.cli import upload_dataset
 from skit_labels import constants as labels_constants
 from skit_pipelines import constants as pipeline_constants
 from skit_pipelines.components.tag_calls.base import Response
+from skit_pipelines.components.download_from_s3 import download_from_s3
 
 
 def upload2labelstudio(input_file: str, project_id: str, response: Response):
+    _, save_path = tempfile.mkstemp(suffix=".csv")
+    download_from_s3(storage_path=input_file, storage_options="", output_path=save_path)
     errors, df_size = upload_dataset(
-        input_file,
+        save_path,
         pipeline_constants.LABELSTUDIO_SVC,
         pipeline_constants.LABELSTUDIO_TOKEN,
         project_id,
