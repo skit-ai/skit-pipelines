238cb57a39eee91d2e26aa873c181dab2cf32d82
ltbringer <amresh.venugopal@gmail.com>
2022-06-09 14:14:28 +0530

    style: typo.

--- skit_pipelines/pipelines/fetch_n_tag_calls/__init__.py
+++ skit_pipelines/pipelines/fetch_n_tag_calls/__init__.py
@@ -50,7 +50,7 @@ def fetch_n_tag_calls(
                 "lang": "en",
                 "end_date": "2020-01-01",
                 "reported": false,
-                "jobs_ids": "4011,4012,4013",
+                "job_ids": "4011,4012,4013",
                 "call_quantity": 200,
                 "notify": "@person, @personwith.spacedname",
                 "channel": "#some-public-channel"
@@ -76,38 +76,55 @@ def fetch_n_tag_calls(
 
     :param client_id: The client id as per api-gateway.
     :type client_id: int
+
     :param org_id: The organization id as per api-gateway.
     :type org_id: str
+
     :param job_ids: The job ids as per tog. Optional if labestudio project id is provided.
     :type job_ids: str
+
     :param labelstudio_project_id: The labelstudio project id (this is a number) since this is optional, defaults to "".
     :type labelstudio_project_id: str
+
     :param start_date: The start date range to filter calls in YYYY-MM-DD format.
     :type start_date: str
+
     :param lang: The language code of the calls to filter. eg: en, hi, ta, te, etc.
     :type lang: str
+
     :param end_date: The end date range to filter calls in YYYY-MM-DD format.
     :type end_date: str
+
     :param ignore_callers: Comma separated list of callers to ignore, defaults to ""
     :type ignore_callers: str, optional
+
     :param reported: Pick only reported calls, defaults to False
     :type reported: bool
+
     :param use_case: Voice bot project's use-case, defaults to ""
     :type use_case: str, optional
+
     :param flow_name: Identifier for a whole/part of a voicebot conversation flow, defaults to ""
     :type flow_name: str, optional
+
     :param min_duration: Call duration filter, defaults to ""
     :type min_duration: str, optional
+
     :param asr_provider: The ASR vendor (google/VASR), defaults to ""
     :type asr_provider: str, optional
+
     :param states: Filter calls in a comma separated list of states, defaults to ""
     :type states: str, optional
+
     :param call_quantity: Number of calls to sample, defaults to 200
     :type call_quantity: int, optional
+
     :param call_type: inbound, outbound vs subtesting call filters. We can currently choose only one of these, defaults to "inbound"
     :type call_type: str, optional
+
     :param notify: Whether to send a slack notification, defaults to ""
     :type notify: str, optional
+
     :param channel: The slack channel to send the notification, defaults to ""
     :type channel: str, optional
     """
