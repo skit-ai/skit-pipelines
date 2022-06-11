04e3ca023e827825d1f369d9276ec5ea38e12628
ltbringer <amresh.venugopal@gmail.com>
2022-06-11 22:44:11 +0530

    refactor: return only error and df size.

--- skit_pipelines/components/tag_calls/labelstudio.py
+++ skit_pipelines/components/tag_calls/labelstudio.py
@@ -5,20 +5,17 @@ from skit_labels.cli import upload_dataset
 
 from skit_pipelines import constants as pipeline_constants
 from skit_pipelines.components.download_from_s3 import download_from_s3
-from skit_pipelines.components.tag_calls.base import Response
+from skit_pipelines.types.tag_calls import TaggingResponse
 
 
-def upload2labelstudio(input_file: str, project_id: str, response: Response):
+def upload2labelstudio(input_file: str, project_id: str):
     _, save_path = tempfile.mkstemp(suffix=".csv")
     download_from_s3(storage_path=input_file, storage_options="", output_path=save_path)
-    errors, df_size = upload_dataset(
+    error, df_size = upload_dataset(
         save_path,
         pipeline_constants.LABELSTUDIO_SVC,
         pipeline_constants.LABELSTUDIO_TOKEN,
         project_id,
         labels_constants.SOURCE__LABELSTUDIO,
     )
-    if errors:
-        response.errors.extend(errors)
-    response.df_sizes.append(df_size)
-    return response
+    return str(error), df_size
--- skit_pipelines/components/tag_calls/tog.py
+++ skit_pipelines/components/tag_calls/tog.py
@@ -4,16 +4,17 @@ from skit_labels import constants as labels_constants
 from skit_labels.cli import upload_dataset
 
 from skit_pipelines import constants as pipeline_constants
-from skit_pipelines.components.tag_calls.base import Response
 
 
-def upload2tog(input_file: str, token: str, job_ids: List[str], response: Response):
+def upload2tog(input_file: str, token: str, job_ids: List[str]):
     url = pipeline_constants.CONSOLE_API_URL
+    errors = []
+    df_sizes = []
     for job_id in job_ids:
         errors, df_size = upload_dataset(
             input_file, url, token, job_id, labels_constants.SOURCE__DB
         )
-    if errors:
-        response.errors.extend(errors)
-        response.df_sizes.append(df_size)
-    return response
+        if errors:
+            errors.extend(errors)
+        df_sizes.append(df_size)
+    return errors, df_sizes
