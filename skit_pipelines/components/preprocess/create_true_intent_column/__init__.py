84c27b0085575fc6616a049c9f72e36191c1bceb
Jaivarsan B <jaiimmortal@gmail.com>
2022-09-21 17:40:06 +0530

    better way to handle the differences in `tag` column

--- skit_pipelines/components/preprocess/create_true_intent_column/__init__.py
+++ skit_pipelines/components/preprocess/create_true_intent_column/__init__.py
@@ -7,8 +7,6 @@ from skit_pipelines import constants as pipeline_constants
 def create_intent_labels(
     data_path: InputPath(str), 
     output_path: OutputPath(str),
-    tog_job_id = None,
-    labelstudio_project_id = None
     ):
     import pandas as pd
     from loguru import logger
@@ -21,15 +19,8 @@ def create_intent_labels(
     INTENT_Y = pipeline_constants.INTENT_Y
 
     train_df = pd.read_csv(data_path)
-
-    # preprocessing step needs to be skipped for labelstudio
-    # didn't do the `if tog_job_id:` to have backward compatability
-    # with tanmay's `eval_xlmr_pipeline`
-    if labelstudio_project_id:
-        pass
-    else:
-        train_df[INTENT_Y] = train_df.tag.apply(pick_1st_tag)
-        logger.debug(train_df.intent_y[:10])
+    train_df[INTENT_Y] = train_df.tag.apply(pick_1st_tag)
+    logger.debug(train_df.intent_y[:10])
     
     train_df.to_csv(output_path, index=False)
 
@@ -37,3 +28,8 @@ def create_intent_labels(
 create_true_intent_labels_op = kfp.components.create_component_from_func(
     create_intent_labels, base_image=pipeline_constants.BASE_IMAGE
 )
+
+
+if __name__ == "__main__":
+
+    create_intent_labels("87.csv", "bleh.csv")
--- skit_pipelines/components/preprocess/create_true_intent_column/utils.py
+++ skit_pipelines/components/preprocess/create_true_intent_column/utils.py
@@ -2,9 +2,30 @@ import json
 
 
 def pick_1st_tag(tag: str):
+
     try:
         tag = json.loads(tag)
-        tag, *_ = json.loads(tag) if isinstance(tag, str) else tag
-        return tag.get("type")
+
+        # if tag was applied json twice while serializing
+        if isinstance(tag, str):
+            tag = json.loads(tag)
+        
+        # since lablestudio and tog can have more than one tags
+        # for the same datapoint, we access only the first element
+        # from the json array
+        if isinstance(tag, list):
+            tag = tag[0]
+
+
+        if isinstance(tag, dict):
+
+            # labelstudio way of extracting the first intent
+            if "choices" in tag:
+                return tag["choices"][0]
+
+            # tog way of extracting the first intent
+            if "type" in tag:
+                return tag["type"]
+
     except json.JSONDecodeError:
         return tag
