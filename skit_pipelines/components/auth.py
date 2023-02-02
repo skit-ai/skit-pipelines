00ab7867e3c3f107a61db818fa7f3281e3eae236
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2023-02-02 11:26:57 +0530

    update: org_auth_token is optional since tog deprecated

--- skit_pipelines/components/auth.py
+++ skit_pipelines/components/auth.py
@@ -7,15 +7,19 @@ from skit_pipelines import constants as pipeline_constants
 
 def org_auth_token(org_id: str, url: Optional[str] = None) -> str:
     from skit_auth import auth, utils
-
+    from loguru import logger
     from skit_pipelines import constants as const
 
     utils.configure_logger(7)
 
     url = const.CONSOLE_API_URL if not url else url
-    token = auth.get_org_token(
-        url, const.CONSOLE_IAM_EMAIL, const.CONSOLE_IAM_PASSWORD, int(org_id)
-    )
+    try:
+        token = auth.get_org_token(
+            url, const.CONSOLE_IAM_EMAIL, const.CONSOLE_IAM_PASSWORD, int(org_id)
+        )
+    except ValueError as exc:
+        logger.error(f"Failed to get org token.\n{exc=}")
+        token = ""
     return token
 
 
