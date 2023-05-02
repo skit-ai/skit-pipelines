f583bc79e25a782906b2c78a0e50279242b86858
Jaivarsan <21098186+greed2411@users.noreply.github.com>
2023-05-02 10:41:13 +0530

    adding support for Studio Calls for US call & slot eval tagging. (#105)

--- skit_pipelines/components/upload_for_call_and_slot_tagging/__init__.py
+++ skit_pipelines/components/upload_for_call_and_slot_tagging/__init__.py
@@ -17,13 +17,21 @@ def fetch_calls_for_slots(
     from skit_pipelines import constants as pipeline_constants
     from skit_pipelines.components import upload2s3
 
-    df = pd.read_csv(untagged_records_path, usecols=["call_uuid", "reftime", "call_type", "language", "call_end_status", "disposition", "flow_id", "flow_version", "flow_name", "call_duration"])
+    df = pd.read_csv(untagged_records_path, usecols=["call_uuid", "reftime", "call_type", "language", "call_end_status", "disposition", "flow_id", "flow_version", "flow_name", "call_duration", "flow_uuid", "client_uuid", "template_id"])
     df = df.drop_duplicates(subset=["call_uuid"])
 
     df["date"] = df["reftime"].apply(lambda x: parser.isoparse(x).strftime("%Y-%m-%d"))
-    df["call_link"] = df["call_uuid"].apply(
-        lambda x: f"{pipeline_constants.CONSOLE_URL}/{org_id}/call-report/#/call?uuid={x}"
-    )
+
+    if pipeline_constants.REGION == pipeline_constants.AP_SOUTH_1:
+        df["call_link"] = df["call_uuid"].apply(
+            lambda x: f"{pipeline_constants.CONSOLE_URL}/{org_id}/call-report/#/call?uuid={x}"
+        )
+    else:
+        df["call_link"] = df.apply(
+            lambda x: f"{pipeline_constants.STUDIO_URL}/clients/{x.client_uuid}/voice-agents/{x.flow_uuid}/calls/{x.call_uuid}",
+            axis=1
+        )
+
     df["language"] = language_code
     print(df.head())
     df.to_csv("op.csv", index=False)
@@ -48,5 +56,9 @@ fetch_calls_for_slots_op = kfp.components.create_component_from_func(
 if __name__ == "__main__":
 
     fetch_calls_for_slots(
-        untagged_records_path="./vi_ta.csv", org_id="65", language_code="ta"
+        untagged_records_path="./emeritus_en.csv", org_id="153", language_code="en"
     )
+
+    # fetch_calls_for_slots(
+    #     untagged_records_path="./abhinav_applications_18-20-april.csv", language_code="en-US"
+    # )
