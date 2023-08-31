b257b762ebdc0aa2f5cc5f8804f5a1c50dcd6171
harshit chopra <harshit.chopra@skit.ai>
2023-08-31 17:51:25 +0530

    Delete deprecated pipelines from codebase

--- skit_pipelines/components/__init__.py
+++ skit_pipelines/components/__init__.py
@@ -14,11 +14,6 @@ from skit_pipelines.components.download_from_s3 import (
 )
 from skit_pipelines.components.download_repo import download_repo, download_repo_op
 from skit_pipelines.components.download_yaml import download_yaml_op
-from skit_pipelines.components.eevee_irr_with_yamls import eevee_irr_with_yamls_op
-from skit_pipelines.components.extract_info_from_dataset import (
-    extract_info_from_dataset_op,
-)
-from skit_pipelines.components.extract_tgz import extract_tgz_op
 from skit_pipelines.components.fetch_calls import fetch_calls_op
 from skit_pipelines.components.fetch_gpt_intent_prediction import (
     fetch_gpt_intent_prediction_op,
@@ -32,20 +27,12 @@ from skit_pipelines.components.file_contents_to_markdown_s3 import (
     file_contents_to_markdown_s3_op,
 )
 from skit_pipelines.components.gen_asr_metrics import gen_asr_metrics_op
-from skit_pipelines.components.gen_confusion_matrix import gen_confusion_matrix_op
-from skit_pipelines.components.gen_eer_metrics import gen_eer_metrics_op
-from skit_pipelines.components.gen_irr_metrics import gen_irr_metrics_op
-from skit_pipelines.components.get_preds_voicebot_xlmr import get_preds_voicebot_xlmr_op
 from skit_pipelines.components.identify_compliance_breaches_llm import (
     identify_compliance_breaches_llm_op,
 )
 from skit_pipelines.components.merge_transcription import overlay_transcription_csv_op
 from skit_pipelines.components.modify_tagged_entities import modify_entity_dataset_op
 from skit_pipelines.components.notification import slack_notification_op
-from skit_pipelines.components.preprocess.create_features import create_features_op
-from skit_pipelines.components.preprocess.create_true_intent_column import (
-    create_true_intent_labels_op,
-)
 from skit_pipelines.components.preprocess.create_true_transcript_column import (
     create_true_transcript_labels_op,
 )
@@ -61,17 +48,13 @@ from skit_pipelines.components.preprocess.process_true_transcript_column import 
 from skit_pipelines.components.push_compliance_report_to_postgres import (
     push_compliance_report_to_postgres_op,
 )
-from skit_pipelines.components.push_eer_to_postgres import push_eer_to_postgres_op
-from skit_pipelines.components.push_irr_to_postgres import push_irr_to_postgres_op
 from skit_pipelines.components.read_json_key import read_json_key_op
 from skit_pipelines.components.retrain_slu_from_repo import retrain_slu_from_repo_op
 from skit_pipelines.components.retrain_slu_from_repo_old import (
     retrain_slu_from_repo_op_old,
 )
 from skit_pipelines.components.tag_calls import tag_calls_op
-from skit_pipelines.components.train_voicebot_xlmr import train_voicebot_xlmr_op
 from skit_pipelines.components.upload2s3 import upload2s3, upload2s3_op
-from skit_pipelines.components.upload2sheet import upload2sheet, upload2sheet_op
 from skit_pipelines.components.upload_for_call_and_slot_tagging import (
     fetch_calls_for_slots_op,
 )
--- skit_pipelines/pipelines/__init__.py
+++ skit_pipelines/pipelines/__init__.py
@@ -1,37 +1,13 @@
 from skit_pipelines.pipelines.asr_tune import asr_tune
-from skit_pipelines.pipelines.eer_from_tog import eer_from_tog
 from skit_pipelines.pipelines.eval_asr_pipeline import eval_asr_pipeline
-from skit_pipelines.pipelines.eval_voicebot_xlmr_pipeline import (
-    eval_voicebot_xlmr_pipeline,
-)
-from skit_pipelines.pipelines.fetch_calls_n_push_to_sheets import (
-    fetch_calls_n_push_to_sheets,
-)
-from skit_pipelines.pipelines.fetch_calls_n_upload_tog_and_sheet import (
-    fetch_calls_n_upload_tog_and_sheet,
-)
 from skit_pipelines.pipelines.fetch_calls_pipeline import fetch_calls_pipeline
 from skit_pipelines.pipelines.fetch_n_tag_calls import fetch_n_tag_calls
-from skit_pipelines.pipelines.fetch_n_tag_turns_and_calls import (
-    fetch_n_tag_turns_and_calls,
-)
-from skit_pipelines.pipelines.fetch_tagged_calls_dataset import (
-    fetch_tagged_calls_dataset,
-)
-from skit_pipelines.pipelines.fetch_tagged_data_from_labelstore import (
-    fetch_tagged_data_from_labelstore,
-)
-from skit_pipelines.pipelines.fetch_tagged_entity_dataset import (
-    fetch_tagged_entity_dataset,
-)
-from skit_pipelines.pipelines.irr_from_tog import irr_from_tog
-from skit_pipelines.pipelines.publish_compliance_breaches import (
-    publish_compliance_breaches,
-)
+from skit_pipelines.pipelines.fetch_n_tag_turns_and_calls import fetch_n_tag_turns_and_calls
+from skit_pipelines.pipelines.fetch_tagged_calls_dataset import fetch_tagged_calls_dataset
+from skit_pipelines.pipelines.fetch_tagged_data_from_labelstore import fetch_tagged_data_from_labelstore
+from skit_pipelines.pipelines.fetch_tagged_entity_dataset import fetch_tagged_entity_dataset
+from skit_pipelines.pipelines.publish_compliance_breaches import publish_compliance_breaches
 from skit_pipelines.pipelines.retrain_slu import retrain_slu
 from skit_pipelines.pipelines.retrain_slu_old import retrain_slu_old
 from skit_pipelines.pipelines.tag_calls import tag_calls
-from skit_pipelines.pipelines.train_voicebot_intent_model_xlmr import (
-    train_voicebot_intent_model_xlmr,
-)
 from skit_pipelines.pipelines.transcription_pipeline import transcription_pipeline
